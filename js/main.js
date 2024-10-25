558f791daa2cd2071e78da04ffc35d8d8597aad8
GemelliAngelo <gemeangelo27@gmail.com>
2024-10-25 14:52:19 +0200

    add snack 2 obj

--- js/main.js
+++ js/main.js
@@ -66,6 +66,29 @@ console.log(`La bici più leggera pesa ${lightestBikevalue} kg`);
 // * Generare numeri random al posto degli 0 nelle proprietà "punti" fatti e "falli subiti".
 // * Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
+let soccerTeams = [
+  {
+    name: "Milan",
+    points: 0,
+    fouls: 0,
+  },
+  {
+    name: "Inter",
+    points: 0,
+    fouls: 0,
+  },
+  {
+    name: "Napoli",
+    points: 0,
+    fouls: 0,
+  },
+  {
+    name: "Roma",
+    points: 0,
+    fouls: 0,
+  },
+];
+
 // ! ESECUZIONE
 
 // ! CONSEGNA
